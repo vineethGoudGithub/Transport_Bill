80e1b33e5ab0086cdb96c9873753859b8d93bc33
vineeth970 <vineeththallapally7890@gmail.com>
2026-05-16 11:15:58 +0530

    fixed frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://transport-billing-system-9hyj.onrender.com/api',
+  baseURL: 'https://transport-bill-5.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
